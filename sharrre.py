322d18ef1d99b0ec61e9c587f4a022565de06d59
Souen Boniface <souenboniface@gmail.com>
2013-02-13 17:34:04 -0500

    Add private function _get_url() to factorize sharrre.py a bit

--- sharrre.py
+++ sharrre.py
@@ -20,9 +20,7 @@ def get_count(channel=None, url=None):
 		# where "4.6k" is the count we are looking for.
 		GOOGLEPLUS_REGEXP = r'aggregateCount".*>(?P<count>[0-9\.kK]+)<'
 		GOOGLEPLUS_URL = "https://plusone.google.com/u/0/_/+1/fastbutton?url=%s&count=true"
-		response = urllib2.urlopen(GOOGLEPLUS_URL % (url,), None, 10)
-		raw_html = response.read()
-		response.close()
+		raw_html = _get_url(GOOGLEPLUS_URL % (url,))
 		match_obj = re.search(GOOGLEPLUS_REGEXP, raw_html)
 		if match_obj is not None:
 			data["count"] = match_obj.groupdict()["count"]
@@ -32,9 +30,16 @@ def get_count(channel=None, url=None):
 
 	elif channel == "stumbleupon":
 		STUMBLEUPON_URL = "http://www.stumbleupon.com/services/1.01/badge.getinfo?url=%s"
-		response = urllib2.urlopen(STUMBLEUPON_URL % (url,), None, 10)
-		response_json = json.loads(response.read())
-		response.close()
-		data["count"] = response_json["result"].get("views", 0)
+		response = _get_url(STUMBLEUPON_URL % (url,))
+		response = json.loads(response)
+		data["count"] = response["result"].get("views", 0)
 
 	return data
+
+
+def _get_url(url):
+	response = urllib2.urlopen(url, None, 10)
+	raw_response = response.read()
+	response.close()
+
+	return raw_response
